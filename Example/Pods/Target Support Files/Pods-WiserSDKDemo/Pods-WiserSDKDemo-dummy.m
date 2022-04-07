5ead0c2c62b2ef89680067555e524bd1268bba09
huangdaxia <huangkai@tuya.com>
2022-04-07 22:33:30 +0800

    remove pods

--- Example/Pods/Target Support Files/Pods-WiserSDKDemo/Pods-WiserSDKDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WiserSDKDemo : NSObject
-@end
-@implementation PodsDummy_Pods_WiserSDKDemo
-@end
--- Example/Pods/Target Support Files/WiserSmartBaseKit/WiserSmartBaseKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WiserSmartBaseKit : NSObject
-@end
-@implementation PodsDummy_WiserSmartBaseKit
-@end
